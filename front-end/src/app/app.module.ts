4222c9514f32362cdac90af9ea02c1b8a394f69b
NaLeum <wognsdl@likelion.org>
2021-08-09 02:44:32 +0900

    app module 설명

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+// AppModule 를 꾸며주겠다.
+// Module 은 독립 가능한 기능들의 상자이다. 
 @NgModule({
+  // 선언값
   declarations: [
     AppComponent
   ],
+  // 다른 모듈들
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // 서비스로직
   providers: [],
+  // 처음 실행한 컴포넌트
   bootstrap: [AppComponent]
 })
 export class AppModule { }
