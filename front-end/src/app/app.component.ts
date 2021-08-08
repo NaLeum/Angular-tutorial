61e1f96d019b53003c3f569cb12fc5b2477074a6
NaLeum <wognsdl@likelion.org>
2021-08-09 02:46:15 +0900

    app component 설명

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+// AppComponent를 컴포넌트로 만들어주는 앵귤러의 api  
 @Component({
+  // 컴포넌트의 태그 네임
   selector: 'app-root',
+  // html 파일
   templateUrl: './app.component.html',
+  // 스타일 css
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
